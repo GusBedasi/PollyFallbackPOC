41cf217d7f0fc0b50b08a34d95902810e0cfada8
GusBedasi <23214684+GusBedasi@users.noreply.github.com>
2023-03-11 11:28:15 -0300

    implemented IoC on fallback policy to receive it through DI

--- PollyFallbackPOC/Services/UsersService.cs
+++ PollyFallbackPOC/Services/UsersService.cs
@@ -1,44 +1,26 @@
 ﻿using Polly;
-using Polly.Fallback;
 
 namespace PollyFallbackPOC.Services
 {
     public class UsersService : IUsersService
     {
         private readonly HttpClient _httpClient;
-
-        public UsersService(HttpClient httpClient)
+        private readonly IAsyncPolicy<string> _policy;
+        public UsersService(HttpClient httpClient, IAsyncPolicy<string> policy)
         {
             _httpClient = httpClient;
+            _policy = policy;
         }
 
         public async Task<string> GetUsers(CancellationToken cancellationToken)
         {
-            //Creates a fallback policy
-            var policy = CreateFallbackPolicy();
-
             //Executes the request which will return an error because of a typo on the resource name
-            var response = await policy.ExecuteAsync(async () =>
+            var response = await _policy.ExecuteAsync(async () =>
             {
                 return await _httpClient.GetStringAsync("/userss", cancellationToken);
             });
 
             return response;
-        }
-
-        private AsyncFallbackPolicy<string> CreateFallbackPolicy()
-        {
-            var policy = Policy<string>
-             .Handle<Exception>()
-             .FallbackAsync(async (cancellationToken) =>
-             {
-                 //Execute the request but this time with the right resource
-                 var response = await _httpClient.GetStringAsync("/users", cancellationToken);
-
-                 return response;
-             });
-
-            return policy;
         }
     }
 }
