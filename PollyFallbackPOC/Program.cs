e3397834c7b71d13b07d372a5ee803c22774807d
GusBedasi <23214684+GusBedasi@users.noreply.github.com>
2023-03-11 11:28:44 -0300

    added log to show requests and responses of integration with jsonplaceholder

--- PollyFallbackPOC/Program.cs
+++ PollyFallbackPOC/Program.cs
@@ -1,6 +1,11 @@
+using Polly;
+using Serilog;
 using PollyFallbackPOC.Services;
+using Serilog.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog();
 
 // Add services to the container.
 
@@ -9,7 +14,39 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddHttpClient<IUsersService, UsersService>();
+builder.Services.AddSingleton<ILoggerFactory>(loggerFactory =>
+{
+    var serilogLogger = new LoggerConfiguration()
+        .MinimumLevel.Information()
+        .WriteTo.Console()
+        .CreateLogger();
+
+    return new SerilogLoggerFactory(serilogLogger);
+});
+
+builder.Services.AddHttpClient<IUsersService, UsersService>(client =>
+{
+    client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com");
+});
+
+builder.Services.AddSingleton<IAsyncPolicy<string>>(serviceProvider =>
+{
+    return Policy<string>
+        .Handle<Exception>()
+        .FallbackAsync(async (cancellationToken) =>
+        {
+            var logger = serviceProvider.GetRequiredService<ILogger<UsersService>>();
+            var httpClientFactory = serviceProvider.GetRequiredService<IHttpClientFactory>();
+            var httpClient = httpClientFactory.CreateClient();
+
+            httpClient.BaseAddress = new Uri("https://jsonplaceholder.typicode.com");
+
+            //Execute the request but this time with the right resource
+            var response = await httpClient.GetStringAsync("/users", cancellationToken);
+
+            return response;
+        });
+});
 
 var app = builder.Build();
 
